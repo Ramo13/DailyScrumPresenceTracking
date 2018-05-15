46f6f93437fb1216615887f0c55d5257648e4a94
Haris Tankovic <hare.tankovic@gmail.com>
2018-05-15 20:05:49 +0200

    added database configuration

--- queries.js
+++ queries.js
@@ -6,7 +6,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://postgres:123456@localhost:5432/presencetracker';
+var connectionString = process.env.DATABASE_URL || 'postgres://postgres:123456@localhost:5432/presencetracker';
 var db = pgp(connectionString);
 
 // add query functions
