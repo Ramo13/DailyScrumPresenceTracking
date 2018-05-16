c7a40065a6f89a2e02b6b2878a133c1f7423e9ca
Haris Tankovic <hare.tankovic@gmail.com>
2018-05-16 14:48:56 +0200

    add create meeting

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ var db = require('../queries');
 router.get('/api/workers', db.getAllWorkers);
 router.get('/api/meetings', db.getAllMeetings);
 router.post('/api/workers', db.createWorker);
+router.post('/api/meetings', db.createMeeting)
 router.put('/api/workers/:id', db.updateWorker);
 router.delete('/api/workers/:id', db.removeWorker);
 
